7fe7a4ce0be9f64ac993332436d30cb575b69479
Shambhu Kumar <shambhu384@gmail.com>
2018-01-09 14:15:38 +0530

    Added faker with twig

--- app.php
+++ app.php
@@ -2,11 +2,33 @@
 
 require 'vendor/autoload.php';
 
-$loader = new Twig_Loader_Filesystem('templates');
+use Symfony\Component\Yaml\Yaml;
 
-$twig = new Twig_Environment($loader, array(
-  'cache' => 'cache',
-));
+$loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
+# config/packages/twig.yaml
+# config/packages/twig.yaml
 
-echo $twig->render('index.html', array('name' => 'Scott', 'title' => 'Startpage'));
+$loader->addPath('layout', 'base');
+// Twig configuration http://symfony.com/doc/current/reference/configuration/twig.htm
 
+$config = Yaml::parseFile(__DIR__.'/config.yaml');
+
+
+
+$twig = new Twig_Environment($loader, $config['twig']);
+
+
+
+$core = $twig->getExtension('Twig_Extension_Core');
+
+
+// add extension
+$twig->addExtension(new Twig_Extensions_Extension_Text());
+$twig->addExtension(new Twig_Extensions_Extension_I18n());
+$twig->addExtension(new Twig_Extensions_Extension_Intl());
+$twig->addExtension(new Twig_Extensions_Extension_Array());
+$twig->addExtension(new Twig_Extensions_Extension_Date());
+
+$faker = Faker\Factory::create();
+
+echo $twig->render('index.html.twig', array('app' => $faker));
